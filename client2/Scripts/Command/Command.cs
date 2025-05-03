0f5f4b633bdd5772fbcb1de0abeffec0f1997495
Karol Koś <kosiyyu@gmail.com>
2025-05-03 16:42:11 +0200

    Add new data types

--- client2/Scripts/Command/Command.cs
+++ client2/Scripts/Command/Command.cs
@@ -3,5 +3,8 @@ namespace Command;
 public enum Command : byte
 {
     POSITION = 0,
-    MOVE = 1
+    MOVE = 1,
+    POSITION_RTT = 2,
+    MOVE_RTT = 3,
+    DEFAULT_RTT = 4
 }
--- client2/Scripts/Data/Data.cs
+++ client2/Scripts/Data/Data.cs
@@ -1,8 +1,10 @@
 namespace Data;
 
+using System.Runtime.InteropServices;
 using C = Command;
 using D = Direction;
 
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
 public struct PositionData
 {
     public C.Command CommandID;
@@ -18,6 +20,24 @@ public struct PositionData
     }
 }
 
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct PositionDataRTT
+{
+    public C.Command CommandID;
+    public byte UserID;
+    public float X;
+    public float Y;
+    public float Z;
+    public float RotY;
+    public uint TimestampRTT;
+
+    public override string ToString()
+    {
+        return $"CommandID: {CommandID}, UserID: {UserID}, X: {X}, Y: {Y}, Z: {Z}, RotY: {RotY} | TimestampRTT: {TimestampRTT}";
+    }
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
 public struct MoveData
 {
     public C.Command CommandID;
@@ -28,6 +48,33 @@ public struct MoveData
     public override string ToString()
     {
         return $"CommandID: {CommandID}, UserID: {UserID}, DirectionID: {DirectionID}, Speed: {Speed}";
+    }
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct MoveDataRTT
+{
+    public C.Command CommandID;
+    public byte UserID;
+    public D.Direction DirectionID;
+    public float Speed;
+    public uint TimestampRTT;
+
+    public override string ToString()
+    {
+        return $"CommandID: {CommandID}, UserID: {UserID}, DirectionID: {DirectionID}, Speed: {Speed} | TimestampRTT: {TimestampRTT}";
+    }
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct DefaultRTT
+{
+    public C.Command CommandID;
+    public uint TimestampRTT;
+
+    public override string ToString()
+    {
+        return $"CommandID: {CommandID} | TimestampRTT: {TimestampRTT}";
     }
 }
 
