2fc615230092a120e18fe2c8255f29f68c2b2739
Karol Koś <kosiyyu@gmail.com>
2025-05-03 19:03:46 +0200

    Add new data types for server

--- server/utils/command/command.go
+++ server/utils/command/command.go
@@ -3,6 +3,9 @@ package command
 type Command uint8
 
 const (
-	POSITION Command = iota // 0
-	MOVE                    // 1
+	POSITION     Command = iota // 0
+	MOVE                        // 1
+	POSITION_RTT                // 2
+	MOVE_RTT                    // 3
+	DEFAULT_RTT                 // 4
 )
--- server/utils/data/data.go
+++ server/utils/data/data.go
@@ -14,9 +14,33 @@ type PositionData struct {
 	RotY      float32
 }
 
+type PositionDataRTT struct {
+	CommandID    command.Command
+	UserID       uint8
+	X            float32
+	Y            float32
+	Z            float32
+	RotY         float32
+	TimestampRTT uint32
+}
+
 type MoveData struct {
-	CommandID   command.Command
-	UserID      uint8
-	DirectionID direction.Direction
-	Speed       float32
+	CommandID    command.Command
+	UserID       uint8
+	DirectionID  direction.Direction
+	Speed        float32
+	TimestampRTT uint32
+}
+
+type MoveDataRTT struct {
+	CommandID    command.Command
+	UserID       uint8
+	DirectionID  direction.Direction
+	Speed        float32
+	TimestampRTT uint32
+}
+
+type DefaultRTT struct {
+	CommandID    command.Command
+	TimestampRTT uint32
 }
