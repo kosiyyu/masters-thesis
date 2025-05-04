82953b5d140156699cb8ae014cae0c328bca2b52
Karol Koś <kosiyyu@gmail.com>
2025-05-04 11:50:36 +0200

    Updata server data

--- server/utils/data/data.go
+++ server/utils/data/data.go
@@ -1,9 +1,12 @@
 package data
 
 import (
+	"fmt"
 	"server/utils/command"
 	"server/utils/direction"
 )
+
+//region structs
 
 type PositionData struct {
 	CommandID command.Command
@@ -25,11 +28,10 @@ type PositionDataRTT struct {
 }
 
 type MoveData struct {
-	CommandID    command.Command
-	UserID       uint8
-	DirectionID  direction.Direction
-	Speed        float32
-	TimestampRTT uint32
+	CommandID   command.Command
+	UserID      uint8
+	DirectionID direction.Direction
+	Speed       float32
 }
 
 type MoveDataRTT struct {
@@ -44,3 +46,33 @@ type DefaultRTT struct {
 	CommandID    command.Command
 	TimestampRTT uint32
 }
+
+//endregion
+
+//region print
+
+func (p PositionData) Print() {
+	fmt.Printf("PositionData{CommandID: %v, UserID: %v, X: %v, Y: %v, Z: %v, RotY: %v}\n",
+		p.CommandID, p.UserID, p.X, p.Y, p.Z, p.RotY)
+}
+
+func (p PositionDataRTT) Print() {
+	fmt.Printf("PositionDataRTT{CommandID: %v, UserID: %v, X: %v, Y: %v, Z: %v, RotY: %v, TimestampRTT: %v}\n",
+		p.CommandID, p.UserID, p.X, p.Y, p.Z, p.RotY, p.TimestampRTT)
+}
+
+func (m MoveData) Print() {
+	fmt.Printf("MoveData{CommandID: %v, UserID: %v, DirectionID: %v, Speed: %v}\n",
+		m.CommandID, m.UserID, m.DirectionID, m.Speed)
+}
+
+func (m MoveDataRTT) Print() {
+	fmt.Printf("MoveDataRTT{CommandID: %v, UserID: %v, DirectionID: %v, Speed: %v, TimestampRTT: %v}\n",
+		m.CommandID, m.UserID, m.DirectionID, m.Speed, m.TimestampRTT)
+}
+
+func (d DefaultRTT) Print() {
+	fmt.Printf("DefaultRTT{CommandID: %v, TimestampRTT: %v}\n", d.CommandID, d.TimestampRTT)
+}
+
+//endregion
